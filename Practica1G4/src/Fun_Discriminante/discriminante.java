64e3fa817a02506e5048663e2a2c33ecedce0fd1
Mario Morales <mmario_93@hotmail.es>
2020-03-01 12:24:54 -0600

    Creacion de la clase Discriminante

---
+++ Practica1G4/src/Fun_Discriminante/discriminante.java
@@ -0,0 +1,14 @@
+/*
+ *  Proyecto de Mario Morales
+ *  mmario_93@hotmail.es
+ *  USAC
+ */
+package Fun_Discriminante;
+
+/**
+ *
+ * @author Mario Morales
+ */
+public class discriminante {
+    
+}
