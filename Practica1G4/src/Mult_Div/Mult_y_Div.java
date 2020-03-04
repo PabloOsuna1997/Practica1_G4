f223d1d06bc2229cdc5147b3ab68cedf4afe3b52
Luis Pablo <wichos11.ch@gmail.com>
2020-03-04 17:48:22 -0600

    numeros primos divisibles exacto multiplicacion

--- Practica1G4/src/Mult_Div/Mult_y_Div.java
+++ Practica1G4/src/Mult_Div/Mult_y_Div.java
@@ -13,8 +13,6 @@ import java.text.DecimalFormat;
  */
 public class Mult_y_Div {
 
-    
-    
     public Mult_y_Div() {
     }
 
@@ -28,8 +26,13 @@ public class Mult_y_Div {
     private void multiplicacion(double numero1, double numero2) {
         double resultado = numero1 * numero2;
 
-        System.out.print("La multiplicacion de " + numero1 + "*" + numero2 + " es " + resultado);
-
+        System.out.println("La multiplicacion de " + numero1 + "*" + numero2 + " es " + resultado);
+        lista="";
+        primos_divisibles_extactos(resultado, 1);
+        if (lista=="") {
+            lista="No tiene";
+        }
+        System.out.println(" Sus numeros divisibles exactos son "+ lista);
         if (numero1 == 0 || numero2 == 0) {     //funcion extra: 201503911
             System.out.println(" -> toda multiplicacion con el numero 0 dara como resultado 0.");
         }
@@ -38,7 +41,7 @@ public class Mult_y_Div {
     public double multiplicacion1(double numero1, double numero2) {
         double resultado = (numero1 * numero2);
         DecimalFormat df = new DecimalFormat("#.0000");
-        
+
         System.out.print("La multiplicacion de " + numero1 + "*" + numero2 + " es " + df.format(resultado));
 
         if (numero1 == 0 || numero2 == 0) {     //funcion extra: 201503911
@@ -62,4 +65,26 @@ public class Mult_y_Div {
             System.out.println("Division: Si el divisor es 0 es resultado siempre dara 0.");
         }
     }
+
+    String lista;
+
+    public String primos_divisibles_extactos(double numero, int contador) {
+        if (contador>numero) {
+            return lista;
+        }
+        double temp = numero / contador;
+        System.out.println(" ---   " +temp%2);
+        if (temp % 2 == 1) {
+
+            lista += contador;
+            contador = contador + 2;
+            return primos_divisibles_extactos(numero, contador);
+        } else {
+            contador = contador + 2;
+            return primos_divisibles_extactos(numero, contador);
+
+        }
+
+    }
+
 }
